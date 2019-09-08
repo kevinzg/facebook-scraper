4b5cda5b7f707242c6526ef0c81af67494c0ada2
Kevin Zúñiga <kevin.zun@gmail.com>
2019-09-07 23:14:58 -0500

    Bump version to 0.1.4

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/kevinzg/facebook-scraper'
 EMAIL = 'kevin.zun@gmail.com'
 AUTHOR = 'Kevin Zúñiga'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.3'
+VERSION = '0.1.4'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
