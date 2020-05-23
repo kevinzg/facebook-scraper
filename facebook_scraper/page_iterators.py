055627edc74fd1f26ff91198e5095b9e2f86c29f
Kevin Zúñiga <kevin.zun@gmail.com>
2020-05-23 18:22:21 -0500

    Add page_iterator module

---
+++ facebook_scraper/page_iterators.py
@@ -0,0 +1,110 @@
+import json
+import re
+from typing import Iterator, Optional
+
+from . import utils
+from .constants import FB_MOBILE_BASE_URL
+from .typing import URL, Element, Page, RequestFunction, Response
+
+
+def iter_pages(account: str, request_fn: RequestFunction) -> Iterator[Page]:
+    start_url = utils.urljoin(FB_MOBILE_BASE_URL, f'/{account}/posts/')
+    return generic_iter_pages(start_url, PageParser, request_fn)
+
+
+def iter_group_pages(group: str, request_fn: RequestFunction) -> Iterator[Page]:
+    start_url = utils.urljoin(FB_MOBILE_BASE_URL, f'groups/{group}/')
+    return generic_iter_pages(start_url, GroupPageParser, request_fn)
+
+
+def generic_iter_pages(start_url, page_parser_cls, request_fn: RequestFunction) -> Iterator[Page]:
+    next_url = start_url
+
+    def get_page(html: Element) -> Iterator[Page]:
+        return html.find('article')
+
+    while next_url:
+        response = request_fn(next_url)
+        parser = page_parser_cls(response)
+
+        html = parser.get_html()
+        yield get_page(html)
+
+        next_url = None
+        next_page = parser.get_next_page()
+        if next_page:
+            next_url = utils.urljoin(FB_MOBILE_BASE_URL, next_page)
+
+
+class PageParser:
+    json_prefix = 'for (;;);'
+
+    cursor_regex = re.compile(r'href:"(/page_content[^"]+)"')  # First request
+    cursor_regex_2 = re.compile(r'href":"(\\/page_content[^"]+)"')  # Other requests
+
+    def __init__(self, response: Response):
+        self.response = response
+        self.html = None
+        self.cursor_blob = None
+
+        self._parse()
+
+    def get_html(self) -> Element:
+        return self.html
+
+    def get_next_page(self) -> Optional[URL]:
+        match = self.cursor_regex.search(self.cursor_blob)
+        if match:
+            return match.groups()[0]
+
+        match = self.cursor_regex_2.search(self.cursor_blob)
+        if match:
+            value = match.groups()[0]
+            return value.encode('utf-8').decode('unicode_escape').replace('\\/', '/')
+
+        return None
+
+    def _parse(self):
+        if self.response.text.startswith(self.json_prefix):
+            self._parse_json()
+        else:
+            # TODO: Why are we uncommenting HTML?
+            self.html = utils.make_html_element(
+                self.response.text.replace('<!--', '').replace('-->', ''), url=self.response.url,
+            )
+            self.cursor_blob = self.response.text
+
+    def _parse_json(self):
+        prefix_length = len(self.json_prefix)
+        data = json.loads(self.response.text[prefix_length:])  # Remove 'for (;;);'
+
+        for action in data['payload']['actions']:
+            if action['cmd'] == 'replace':
+                self.html = utils.make_html_element(action['html'], url=FB_MOBILE_BASE_URL)
+            elif action['cmd'] == 'script':
+                self.cursor_blob = action['code']
+
+
+class GroupPageParser(PageParser):
+    cursor_regex_3 = re.compile(r'\shref="(\/groups\/[^"]+bac=[^"]+)"')  # for Group requests
+
+    def get_html(self) -> Element:
+        return self.html
+
+    def get_next_page(self) -> Optional[URL]:
+        next_page = super().get_next_page()
+        if next_page:
+            return next_page
+
+        match = self.cursor_regex_3.search(self.response)
+        if match:
+            value = match.groups()[0]
+            return value.encode('utf-8').decode('unicode_escape').replace('\\/', '/')
+
+        return None
+
+    def _parse(self):
+        self.html = utils.make_html_element(
+            self.response.text.replace('<!--', '').replace('-->', ''), url=self.response.url,
+        )
+        self.cursor_blob = self.response.text
