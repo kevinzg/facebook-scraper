51a77f56c040abbe4291df22ec67b0bc9dfaf8c7
Kevin Zúñiga <kevin.zun@gmail.com>
2020-05-23 19:10:55 -0500

    Move extract_post to extractors module

---
+++ facebook_scraper/extractors.py
@@ -0,0 +1,298 @@
+import itertools
+import json
+import logging
+import re
+from datetime import datetime
+from json import JSONDecodeError
+from typing import Any, Dict, Optional
+
+from . import utils
+from .constants import FB_MOBILE_BASE_URL
+from .typing import Element, Options, Post, RequestFunction
+
+
+logger = logging.getLogger(__name__)
+
+# Typing
+PartialPost = Optional[Dict[str, Any]]
+
+
+def extract_post(element: Element, options: Options, request_fn: RequestFunction) -> Post:
+    return PostExtractor(element, options, request_fn).extract_post()
+
+
+class PostExtractor:
+    likes_regex = re.compile(r'like_def[^>]*>([0-9,.]+)')
+    comments_regex = re.compile(r'cmt_def[^>]*>([0-9,.]+)')
+    shares_regex = re.compile(r'([0-9,.]+)\s+Shares', re.IGNORECASE)
+    link_regex = re.compile(r"href=\"https:\/\/lm\.facebook\.com\/l\.php\?u=(.+?)\&amp;h=")
+
+    photo_link = re.compile(r'href=\"(/[^\"]+/photos/[^\"]+?)\"')
+    image_regex = re.compile(
+        r'<a href=\"([^\"]+?)\" target=\"_blank\" class=\"sec\">View Full Size<\/a>',
+        re.IGNORECASE,
+    )
+    image_regex_lq = re.compile(r"background-image: url\('(.+)'\)")
+    post_url_regex = re.compile(r'/story.php\?story_fbid=')
+
+    shares_and_reactions_regex = re.compile(
+        r'<script>.*bigPipe.onPageletArrive\((?P<data>\{.*RelayPrefetchedStreamCache.*\})\);'
+        '.*</script>'
+    )
+    bad_json_key_regex = re.compile(r'(?P<prefix>[{,])(?P<key>\w+):')
+
+    more_url_regex = re.compile(r'(?<=…\s)<a href="([^"]+)')
+    post_story_regex = re.compile(r'href="(\/story[^"]+)" aria')
+
+    def __init__(self, element, options, request_fn):
+        self.element = element
+        self.options = options
+        self.request = request_fn
+
+        self._data_ft = None
+
+    def extract_post(self) -> Post:
+        """Parses the element into self.item"""
+
+        methods = [
+            self.extract_post_id,
+            self.extract_text,
+            self.extract_time,
+            self.extract_image,
+            self.extract_likes,
+            self.extract_comments,
+            self.extract_shares,
+            self.extract_post_url,
+            self.extract_link,
+        ]
+
+        post = {}
+
+        self.post = post
+
+        for method in methods:
+            try:
+                partial_post = method()
+                if partial_post is None:
+                    logger.warning("Extract method %s didn't return anythign", method.__name__)
+                    continue
+
+                post.update(partial_post)
+            except Exception as ex:
+                logger.exception("Exception while running %s: %s", method.__name__, ex)
+                raise
+
+        if 'reactions' in self.options:
+            reactions = self.extract_reactions()
+            post.update(reactions)
+
+        return post
+
+    def extract_post_id(self) -> PartialPost:
+        return {'post_id': self.data_ft.get('mf_story_key')}
+
+    # TODO: this method needs test for the 'has more' case and shared content
+    def extract_text(self) -> PartialPost:
+        # Open this article individually because not all content is fully loaded when skimming
+        # through pages.
+        # This ensures the full content can be read.
+
+        element = self.element
+
+        has_more = self.more_url_regex.search(element.html)
+        if has_more:
+            match = self.post_story_regex.search(element.html)
+            if match:
+                url = utils.urljoin(FB_MOBILE_BASE_URL, match.groups()[0].replace("&amp;", "&"))
+                response = self.request(url)
+                element = response.html.find('.story_body_container', first=True)
+
+        nodes = element.find('p, header')
+        if nodes:
+            post_text = []
+            shared_text = []
+            ended = False
+            for node in nodes[1:]:
+                if node.tag == 'header':
+                    ended = True
+
+                # Remove '... More'
+                # This button is meant to display the hidden text that is already loaded
+                # Not to be confused with the 'More' that opens the article in a new page
+                if node.tag == 'p':
+                    node = utils.make_html_element(
+                        html=node.html.replace('>… <', '><', 1).replace('>More<', '', 1)
+                    )
+
+                if not ended:
+                    post_text.append(node.text)
+                else:
+                    shared_text.append(node.text)
+
+            text = '\n'.join(itertools.chain(post_text, shared_text))
+            post_text = '\n'.join(post_text)
+            shared_text = '\n'.join(shared_text)
+
+            return {
+                'text': text,
+                'post_text': post_text,
+                'shared_text': shared_text,
+            }
+
+        return None
+
+    def extract_time(self) -> PartialPost:
+        page_insights = self.data_ft['page_insights']
+
+        for page in page_insights.values():
+            try:
+                timestamp = page['post_context']['publish_time']
+                return {
+                    'time': datetime.fromtimestamp(timestamp),
+                }
+            except (KeyError, ValueError):
+                continue
+
+    def extract_image(self) -> PartialPost:
+        image_link = self.extract_photo_link()
+        if image_link is not None:
+            return image_link
+        return self.extract_image_lq()
+
+    def extract_image_lq(self) -> PartialPost:
+        story_container = self.element.find('div.story_body_container', first=True)
+        if story_container is None:
+            return {'image': None}
+        other_containers = story_container.xpath('div/div')
+
+        for container in other_containers:
+            image_container = container.find('.img', first=True)
+            if image_container is None:
+                continue
+
+            style = image_container.attrs.get('style', '')
+            match = self.image_regex_lq.search(style)
+            if match:
+                return {'image': utils.decode_css_url(match.groups()[0])}
+
+        return {'image': None}
+
+    def extract_link(self) -> PartialPost:
+        match = self.link_regex.search(self.element.html)
+        link = None
+        if match:
+            link = utils.unquote(match.groups()[0])
+        return {'link': link}
+
+    def extract_post_url(self) -> PartialPost:
+        query_params = ('story_fbid', 'id')
+
+        elements = self.element.find('header a')
+        for element in elements:
+            href = element.attrs.get('href', '')
+            match = self.post_url_regex.match(href)
+            if match:
+                path = utils.filter_query_params(href, whitelist=query_params)
+                url = utils.urljoin(FB_MOBILE_BASE_URL, path)
+                return {'post_url': url}
+
+    # TODO: Remove `or 0` from this methods
+    def extract_likes(self) -> PartialPost:
+        return {
+            'likes': utils.find_and_search(
+                self.element, 'footer', self.likes_regex, utils.parse_int
+            )
+            or 0,
+        }
+
+    def extract_comments(self) -> PartialPost:
+        return {
+            'comments': utils.find_and_search(
+                self.element, 'footer', self.comments_regex, utils.parse_int
+            )
+            or 0,
+        }
+
+    def extract_shares(self) -> PartialPost:
+        return {
+            'shares': utils.find_and_search(
+                self.element, 'footer', self.shares_regex, utils.parse_int
+            )
+            or 0,
+        }
+
+    def extract_photo_link(self) -> str:
+        match = self.photo_link.search(self.element.html)
+        if not match:
+            return None
+
+        url = utils.urljoin(FB_MOBILE_BASE_URL, match.groups()[0])
+
+        response = self.request(url)
+        html = response.text
+        match = self.image_regex.search(html)
+        if match:
+            return {
+                'image': match.groups()[0].replace("&amp;", "&"),
+            }
+
+    def extract_reactions(self) -> PartialPost:
+        """Fetch share and reactions information with a existing post obtained by `get_posts`.
+        Return a merged post that has some new fields including `reactions`, `w3_fb_url`,
+        `fetched_time`, and reactions fields `LIKE`, `ANGER`, `SORRY`, `WOW`, `LOVE`, `HAHA` if
+        exist.
+        Note that this method will raise one http request per post, use it when you want some more
+        information.
+
+        Example:
+        ```
+        for post in get_posts('fanpage'):
+            more_info_post = fetch_share_and_reactions(post)
+            print(more_info_post)
+        ```
+        """
+        url = self.post.get('post_url')
+        post_id = self.post.get('post_id')
+
+        if url:
+            w3_fb_url = utils.urlparse(url)._replace(netloc='www.facebook.com').geturl()
+            resp = self.request(w3_fb_url)
+            for item in self.parse_share_and_reactions(resp.text):
+                data = item['jsmods']['pre_display_requires'][0][3][1]['__bbox']['result'][
+                    'data'
+                ]['feedback']
+                if data['subscription_target_id'] == post_id:
+                    return {
+                        'shares': data['share_count']['count'],
+                        'likes': data['reactors']['count'],
+                        'reactions': {
+                            reaction['node']['reaction_type'].lower(): reaction['reaction_count']
+                            for reaction in data['top_reactions']['edges']
+                        },
+                        'comments': data['comment_count']['total_count'],
+                        'w3_fb_url': data['url'],
+                        'fetched_time': datetime.now(),
+                    }
+        return {}
+
+    def parse_share_and_reactions(self, html: str):
+        bad_jsons = self.shares_and_reactions_regex.findall(html)
+        for bad_json in bad_jsons:
+            good_json = self.bad_json_key_regex.sub(r'\g<prefix>"\g<key>":', bad_json)
+            yield json.loads(good_json)
+
+    @property
+    def data_ft(self) -> dict:
+        if self._data_ft is not None:
+            return self._data_ft
+
+        self._data_ft = {}
+        try:
+            data_ft_json = self.element.attrs['data-ft']
+            self._data_ft = json.loads(data_ft_json)
+        except JSONDecodeError:
+            logger.error("Error parsing data-ft JSON")
+        except KeyError:
+            logger.error("data-ft attribute not found")
+
+        return self._data_ft
--- facebook_scraper/facebook_scraper.py
+++ facebook_scraper/facebook_scraper.py
@@ -1,13 +1,7 @@
-import codecs
-import csv
-import itertools
 import json
 import re
-import sys
 import time
 import warnings
-from datetime import datetime
-from urllib import parse as urlparse
 
 from requests import RequestException
 from requests_html import HTML, HTMLSession
@@ -25,26 +19,9 @@ _headers = {'User-Agent': _user_agent, 'Accept-Language': 'en-US,en;q=0.5', 'coo
 _session = None
 _timeout = None
 
-_likes_regex = re.compile(r'like_def[^>]*>([0-9,.]+)')
-_comments_regex = re.compile(r'cmt_def[^>]*>([0-9,.]+)')
-_shares_regex = re.compile(r'([0-9,.]+)\s+Shares', re.IGNORECASE)
-_link_regex = re.compile(r"href=\"https:\/\/lm\.facebook\.com\/l\.php\?u=(.+?)\&amp;h=")
-
 _cursor_regex = re.compile(r'href:"(/page_content[^"]+)"')  # First request
 _cursor_regex_2 = re.compile(r'href":"(\\/page_content[^"]+)"')  # Other requests
 _cursor_regex_3 = re.compile(r'\shref="(\/groups\/[^"]+bac=[^"]+)"')  # for Group requests
-
-_photo_link = re.compile(r"href=\"(/[^\"]+/photos/[^\"]+?)\"")
-_image_regex = re.compile(r"<a href=\"([^\"]+?)\" target=\"_blank\" class=\"sec\">View Full Size<\/a>", re.IGNORECASE)
-_image_regex_lq = re.compile(r"background-image: url\('(.+)'\)")
-_post_url_regex = re.compile(r'/story.php\?story_fbid=')
-
-_more_url_regex = re.compile(r'(?<=…\s)<a href="([^"]+)')
-_post_story_regex = re.compile(r'href="(\/story[^"]+)" aria')
-
-_shares_and_reactions_regex = re.compile(
-    r'<script>.*bigPipe.onPageletArrive\((?P<data>\{.*RelayPrefetchedStreamCache.*\})\);.*</script>')
-_bad_json_key_regex = re.compile(r'(?P<prefix>[{,])(?P<key>\w+):')
 
 
 def get_posts(account=None, group=None, **kwargs):
@@ -150,150 +127,6 @@ def _get_group_posts(path, pages=10, timeout=5, sleep=0, credentials=None, extra
             time.sleep(sleep)
 
 
-def _extract_post(article):
-    text, post_text, shared_text = _extract_text(article)
-    return {
-        'post_id': _extract_post_id(article),
-        'text': text,
-        'post_text': post_text,
-        'shared_text': shared_text,
-        'time': _extract_time(article),
-        'image': _extract_image(article),
-        'likes': _find_and_search(article, 'footer', _likes_regex, _parse_int) or 0,
-        'comments': _find_and_search(article, 'footer', _comments_regex, _parse_int) or 0,
-        'shares':  _find_and_search(article, 'footer', _shares_regex, _parse_int) or 0,
-        'post_url': _extract_post_url(article),
-        'link': _extract_link(article),
-    }
-
-
-def _extract_post_id(article):
-    try:
-        data_ft = json.loads(article.attrs['data-ft'])
-        return data_ft['mf_story_key']
-    except (KeyError, ValueError):
-        return None
-
-
-def _extract_text(article):
-    # Open this article individually because not all content is fully loaded when skimming through pages
-    # This ensures the full content can be read
-    hasMore = _more_url_regex.search(article.html)
-    if hasMore:
-        match = _post_story_regex.search(article.html)
-        if match:
-            url = f'{_base_url}{match.groups()[0].replace("&amp;", "&")}'
-            response = _session.get(url, timeout=_timeout)
-            article = response.html.find('.story_body_container', first=True)
-
-    nodes = article.find('p, header')
-    if nodes:
-        post_text = []
-        shared_text = []
-        ended = False
-        for node in nodes[1:]:
-            if node.tag == "header":
-                ended = True
-
-            # Remove '... More'
-            # This button is meant to display the hidden text that is already loaded
-            # Not to be confused with the 'More' that opens the article in a new page
-            if node.tag == "p":
-                node = HTML(html=node.html.replace('>… <', '><', 1).replace('>More<', '', 1))
-
-            if not ended:
-                post_text.append(node.text)
-            else:
-                shared_text.append(node.text)
-
-        text = '\n'.join(itertools.chain(post_text, shared_text))
-        post_text = '\n'.join(post_text)
-        shared_text = '\n'.join(shared_text)
-
-        return text, post_text, shared_text
-
-    return None
-
-
-def _extract_time(article):
-    try:
-        data_ft = json.loads(article.attrs['data-ft'])
-        page_insights = data_ft['page_insights']
-    except (KeyError, ValueError):
-        return None
-
-    for page in page_insights.values():
-        try:
-            timestamp = page['post_context']['publish_time']
-            return datetime.fromtimestamp(timestamp)
-        except (KeyError, ValueError):
-            continue
-    return None
-
-
-def _extract_photo_link(article):
-    match = _photo_link.search(article.html)
-    if not match:
-        return None
-
-    url = f"{_base_url}{match.groups()[0]}"
-
-    response = _session.get(url, timeout=_timeout)
-    html = response.html.html
-    match = _image_regex.search(html)
-    if match:
-        return match.groups()[0].replace("&amp;", "&")
-    return None
-
-
-def _extract_image(article):
-    image_link = _extract_photo_link(article)
-    if image_link is not None:
-        return image_link
-    return _extract_image_lq(article)
-
-
-def _extract_image_lq(article):
-    story_container = article.find('div.story_body_container', first=True)
-    if story_container is None:
-        return None
-    other_containers = story_container.xpath('div/div')
-
-    for container in other_containers:
-        image_container = container.find('.img', first=True)
-        if image_container is None:
-            continue
-
-        style = image_container.attrs.get('style', '')
-        match = _image_regex_lq.search(style)
-        if match:
-            return _decode_css_url(match.groups()[0])
-
-    return None
-
-
-def _extract_link(article):
-    html = article.html
-    match = _link_regex.search(html)
-    if match:
-        return urlparse.unquote(match.groups()[0])
-    return None
-
-
-def _extract_post_url(article):
-    query_params = ('story_fbid', 'id')
-
-    elements = article.find('header a')
-    for element in elements:
-        href = element.attrs.get('href', '')
-        match = _post_url_regex.match(href)
-        if match:
-            path = _filter_query_params(href, whitelist=query_params)
-            return f'{_base_url}{path}'
-
-    return None
-
-
 def _find_cursor(text):
     match = _cursor_regex.search(text)
     if match:
@@ -318,50 +151,6 @@ def _login_user(email, password):
     _session.post(_base_url + login_action, data={'email': email, 'pass': password})
     if 'c_user' not in _session.cookies:
         warnings.warn('login unsuccessful')
-
-
-def fetch_share_and_reactions(post: dict):
-    """Fetch share and reactions information with a existing post obtained by `get_posts`.
-    Return a merged post that has some new fields including `reactions`, `w3_fb_url`, `fetched_time`,
-        and reactions fields `LIKE`, `ANGER`, `SORRY`, `WOW`, `LOVE`, `HAHA` if exist.
-
-    Note that this method will raise one http request per post, use it when you want some more information.
-
-    Example:
-    ```
-    for post in get_posts('fanpage'):
-        more_info_post = fetch_share_and_reactions(post)
-        print(more_info_post)
-    ```
-    """
-    url = post.get('post_url')
-    if url:
-        w3_fb_url = urlparse.urlparse(url)._replace(netloc='www.facebook.com').geturl()
-        resp = _session.get(w3_fb_url, timeout=_timeout)
-        for item in _parse_share_and_reactions(resp.text):
-            data = (item['jsmods']['pre_display_requires'][0][3][1]['__bbox']['result']
-                    ['data']['feedback'])
-            if data['subscription_target_id'] == post['post_id']:
-                return {
-                    **post,
-                    'shares': data['share_count']['count'],
-                    'likes': data['reactors']['count'],
-                    'reactions': {
-                        reaction['node']['reaction_type'].lower(): reaction['reaction_count']
-                        for reaction in data['top_reactions']['edges']
-                    },
-                    'comments': data['comment_count']['total_count'],
-                    'w3_fb_url': data['url'],
-                    'fetched_time': datetime.now(),
-                }
-    return post
-
-
-def _parse_share_and_reactions(html: str):
-    bad_jsons = _shares_and_reactions_regex.findall(html)
-    for bad_json in bad_jsons:
-        good_json = _bad_json_key_regex.sub(r'\g<prefix>"\g<key>":', bad_json)
-        yield json.loads(good_json)
 
 
 def write_posts_to_csv(account=None, group=None, filename=None, **kwargs):
