97ce346c79e09b6a71f05e9e75fce8c62039d943
Kevin Zúñiga <kevin.zun@gmail.com>
2020-06-14 21:09:13 -0500

    Add group option to argument parser

--- facebook_scraper/__main__.py
+++ facebook_scraper/__main__.py
@@ -9,10 +9,14 @@ def run():
     parser.add_argument('account', type=str, help="Facebook account")
     parser.add_argument('-f', '--filename', type=str, help="Output filename")
     parser.add_argument('-p', '--pages', type=int, help="Number of pages to download", default=10)
+    parser.add_argument('-g', '--group', action='store_true', help="Use group scraper")
 
     args = parser.parse_args()
 
-    write_posts_to_csv(account=args.account, filename=args.filename, pages=args.pages)
+    account_type = 'group' if args.group else 'account'
+    kwargs = {account_type: args.account}
+
+    write_posts_to_csv(**kwargs, filename=args.filename, pages=args.pages)
 
 
 if __name__ == '__main__':
