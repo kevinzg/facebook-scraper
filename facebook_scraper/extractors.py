fb85adf020b50f1750c8ea385c24f36c523f060e
Nick Young <nick.young@auckland.ac.nz>
2021-06-16 13:41:17 +1200

    tweak likes_regex

--- facebook_scraper/extractors.py
+++ facebook_scraper/extractors.py
@@ -48,7 +48,7 @@ def extract_photo_post(
 class PostExtractor:
     """Class for Extracting fields from a FacebookPost"""
 
-    likes_regex = re.compile(r'([\d,.KM]+)\s+(Like|left reaction)', re.IGNORECASE)
+    likes_regex = re.compile(r'([\d,.KM]+)\s+(Like|left reaction|others reacted)', re.IGNORECASE)
     comments_regex = re.compile(r'([\d,.KM]+)\s+comment', re.IGNORECASE)
     shares_regex = re.compile(r'([\d,.KM]+)\s+Share', re.IGNORECASE)
     live_regex = re.compile(r'.+(is live).+')
