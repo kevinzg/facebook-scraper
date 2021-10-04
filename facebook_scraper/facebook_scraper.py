840c5252e4c22bda2f4506596a244b2325fefc92
Nick Young <nick.young@auckland.ac.nz>
2021-10-04 16:46:27 +1300

    tweak login approval checkpoint handling

--- facebook_scraper/facebook_scraper.py
+++ facebook_scraper/facebook_scraper.py
@@ -558,7 +558,8 @@ class FacebookScraper:
                 response = self.submit_form(response, {"name_action_selected": "save_device"})
 
         if "Login approval needed" in response.text or "checkpoint" in response.url:
-            raise exceptions.LoginError("Login approval needed")
+            input("Login approval needed. From a browser logged into this account, approve this login from your notifications. Press enter once you've approved it.")
+            self.submit_form(response)
         if "The password that you entered is incorrect" in response.text:
             raise exceptions.LoginError("The password that you entered is incorrect")
         if 'c_user' not in self.session.cookies:
