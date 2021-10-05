42e9dee3c30c3592767b7dd0065fbc247bb48a78
Nick Young <nick.young@auckland.ac.nz>
2021-10-05 19:50:35 +1300

    Fallback method for pages that are a blend of profile and page

--- facebook_scraper/facebook_scraper.py
+++ facebook_scraper/facebook_scraper.py
@@ -354,9 +354,16 @@ class FacebookScraper:
                 logger.debug(f"Requesting page from: {url}")
                 resp = self.get(url)
                 desc = resp.html.find("meta[name='description']", first=True)
-                result["about"] = resp.html.find(
-                    '#pages_msite_body_contents>div>div:nth-child(2)', first=True
-                ).text
+                try:
+                    result["about"] = resp.html.find(
+                        '#pages_msite_body_contents>div>div:nth-child(2)', first=True
+                    ).text
+                except Exception as e:
+                    logger.error(e)
+                    result = self.get_profile(page)
+                    result["followers"] = utils.convert_numeric_abbr(resp.html.find(
+                        "span.unlinkedTextEntity", first=True
+                    ).text.replace(" Followers", ""))
             except Exception as e:
                 logger.error(e)
         if desc:
