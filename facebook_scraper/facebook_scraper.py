e71b89132981eff50ef5d8126cd5bc26a718c6d2
Nick Young <nick.young@auckland.ac.nz>
2021-11-20 15:06:58 +1300

    re-add sec-fetch-user header

--- facebook_scraper/facebook_scraper.py
+++ facebook_scraper/facebook_scraper.py
@@ -34,6 +34,7 @@ class FacebookScraper:
     base_url = FB_MOBILE_BASE_URL
     default_headers = {
         'Accept-Language': 'en-US,en;q=0.5',
+        "Sec-Fetch-User": "?1",
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
     }
     have_checked_locale = False
