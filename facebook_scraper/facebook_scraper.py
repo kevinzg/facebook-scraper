f659c567b7cf56a84bde7bf26a6936b68e7a9cc9
Niels Ørbæk Chemnitz <Nielsoerbaek@gmail.com>
2023-01-12 05:42:23 +0100

    Increase user-agent versions to stop inifinite redirect loop (#949)
    * Move members-search inside try-block
    
    * Handle translation for shared and user text, split into separate properties.
    
    * Increase user-agent versions to stop inifinite redirect loop

--- facebook_scraper/facebook_scraper.py
+++ facebook_scraper/facebook_scraper.py
@@ -52,7 +52,7 @@ class FacebookScraper:
         "Accept": "*/*",
         "Connection": "keep-alive",
         "Accept-Encoding": "gzip,deflate",
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15",
     }
     have_checked_locale = False
 
